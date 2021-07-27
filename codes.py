9ca07a7bae479ade2283f54cf8c545139482c83e
geeknow112 <geeknow112@gmail.com>
2021-07-27 13:43:22 +0000

    memo git command

--- codes.py
+++ codes.py
@@ -1,3 +1,27 @@
 def main():
     print('test')
 
+'''
+[root@de2720888fcc git_repo]# history | grep git
+   78  git clone https://github.com/geeknow112/memo.git
+   86  mkdir git_repo
+   87  cd git_repo/
+   88  git clone https://github.com/geeknow112/memo.git
+   93  git add ./codes.py
+   94  git commit codes.py
+   95  git config --global
+   96  git config
+  101  git commit ./codes.py https://github.com/geeknow112/memo.git
+  102  git commit ./codes.py
+  103   git config --global user.name "geeknow112"
+  104  git commit ./codes.py
+  105  git config --global user.email "geeknow112@gmail.com"
+  106  git commit ./codes.py
+  107  git push ./codes.py
+  510  cd git_repo/
+  519  git help
+
+
+0/5 6-8,9-12,13-15,16-21,22-23 * * ? *
+'''
+
